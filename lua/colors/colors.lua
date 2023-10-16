22867f99812cec12463572c83a40f74fd12d119d
erdivartanovich <erdivartanovich@gmail.com>
2023-10-16 20:13:07 +0800

    make it only for pywal colors

--- lua/colors/colors.lua
+++ lua/colors/colors.lua
@@ -1,6 +1,20 @@
+local pywal = require('pywal')
+local colors = pywal.get_colors()
 return {
-    base00 = '#111111', base01 = '#333333', base02 = '#555555', base03 = '#777777',
-    base04 = '#999999', base05 = '#bbbbbb', base06 = '#dddddd', base07 = '#ffffff',
-    base08 = '#ff4136', base09 = '#ff851b', base0A = '#ffdc00', base0B = '#2ecc40',
-    base0C = '#7fdbff', base0D = '#0074d9', base0E = '#b10dc9', base0F = '#85144b'
+    base00 = colors.color0,
+    base01 = colors.color1,
+    base02 = colors.color2,
+    base03 = colors.color3,
+    base04 = colors.color4,
+    base05 = colors.color5,
+    base06 = colors.color6,
+    base07 = colors.color7,
+    base08 = colors.color8,
+    base09 = colors.color9,
+    base0A = colors.color10,
+    base0B = colors.color11,
+    base0C = colors.color12,
+    base0D = colors.color13,
+    base0E = colors.color14,
+    base0F = colors.color15,
 }
